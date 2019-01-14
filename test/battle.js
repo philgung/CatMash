e514fe638c498948375ada637e686dc12b1988ff
philgung <44118230+philgung@users.noreply.github.com>
2019-01-14 17:16:56 +0100

    first tests

--- test/battle.js
+++ test/battle.js
@@ -4,38 +4,63 @@
 // Deuxième round : Ceux qui ont 1 point s’affrontent (pas de conflit), ceux qui ont 0 point s’affrontent (pas de conflit)
 // etc ...
 
+var Battle = require('../js/battle');
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
 
-// createBattle, nextBattle, getRankings
 describe('battle', () => {
-    // beforeEach((done) => { 
-                        
-    // });
-    describe('First Round', () => {
-        it('Players are divided en two sub groups. A sub group S1 composed from players 1 to n/2, and another sub group s2 composed from players (n/2)+1 to n. The first of S1 play against the first of S2, the second of S1 against the second of S2, etc.', (done) => {
-          
-        });
-        it('En cas de nombre impair de participants le dernier est exempté et marque un point', (done) => {
+    describe('nextBattle', () => {
+        describe('Les participants sont divisés en deux sous groupes. Un sous groupe S1 composé de participants 1 à n/2, et un sous groupe s2 composé de participants (n/2)+1 à n. Le premier participant de S1 affronte le premier de S2, le second de S1 affronte le second de S2, etc.', () => {
+          it('Pour 1 seul participant, il est déclaré vainqueur.', () => {
+            var players = ['firstPlayerId'];  
+            var battle = new Battle(players);
+            var nextbattle = battle.nextBattle();
+            expect(nextbattle.isFinished).to.be.true;
+            expect(nextbattle.winnerId).to.be.equal('firstPlayerId');
+          });
+          it('Pour 2 participants, les deux saffrontent.', () => {
+            var players = ['firstPlayerId', 'secondPlayerId'];
+            var battle = new Battle(players);
+            var nextbattle = battle.nextBattle();
+            expect(nextbattle.isFinished).to.be.false;
+            expect(nextbattle.winnerId).to.be.undefined;
+            expect(nextbattle.firstPlayerId).to.be.eql('firstPlayerId');
+            expect(nextbattle.secondPlayerId).to.be.eql('secondPlayerId');
+          });
+          it('Pour 3 participants, les deux premiers saffrontent, le dernier naffronte personne, mais il gagne 1 point.', () => {
+            var players = ['firstPlayerId', 'secondPlayerId', 'thirdPlayerId'];
+            var battle = new Battle(players);
+            var nextbattle = battle.nextBattle();
+            expect(nextbattle.isFinished).to.be.false;
+            expect(nextbattle.winnerId).to.be.undefined;
+            expect(nextbattle.firstPlayerId).to.be.eql('firstPlayerId');
+            expect(nextbattle.secondPlayerId).to.be.eql('secondPlayerId');
+          });
+
+        });        
+        it('En cas de nombre impair de participants le dernier est exempté et marque un point', () => {
 
         });
         
     });
-    describe('Others Rounds', () => {        
-        it('Après chaque ronde, on regroupe les participants ayant le même nombre de points et on recommence le processus décrit ci-dessus en veillant à ne jamais faire se rencontrer deux fois les mêmes adversaires', (done) => {
+    describe('Les Autres rondes', () => {        
+        it('Après chaque ronde, on regroupe les participants ayant le même nombre de points et on recommence le processus décrit ci-dessus en veillant à ne jamais faire se rencontrer deux fois les mêmes adversaires', () => {
 
         });
-        it('Lorsquun participant ne peut être apparié dans son groupe de points (sil est seul ou sil a déjà rencontré tous les autres) il est apparié dans le groupe de points le plus proche.', (done) => {
+        it('Lorsquun participant ne peut être apparié dans son groupe de points (sil est seul ou sil a déjà rencontré tous les autres) il est apparié dans le groupe de points le plus proche.', () => {
 
         });
-        it('aucun joueur ne rencontre deux fois le même adversaire', (done) => {
+        it('aucun joueur ne rencontre deux fois le même adversaire', () => {
 
         });
-        it('Les joueurs qui gagnent reçoivent un point et les perdants ne reçoivent aucun point', (done) => {
+        it('Les joueurs qui gagnent reçoivent un point et les perdants ne reçoivent aucun point', () => {
 
         });
-        it('Après le premier tour, les joueurs affrontent des adversaires qui comptent le même nombre de points (ou à peu près). ', (done) => {
+        it('Après le premier tour, les joueurs affrontent des adversaires qui comptent le même nombre de points (ou à peu près). ', () => {
 
         });
-        it('On ne garde que les vainqueurs', (done) => {
+        it('On ne garde que les vainqueurs', () => {
 
         });
     });
