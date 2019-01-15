bae996a6a4f7bde32b9de6cd495fe189ce4d8aa2
Philippe Gung <philippe.gung@youscribe.com>
2019-01-15 22:02:50 +0100

    implement getNextRound

--- js/tournament.js
+++ js/tournament.js
@@ -1,27 +1,42 @@
 // createBattle, nextBattle, getRankings
 
 
-function Tournament() {
+function Tournament(allPlayers) {
     function Battle(id, firstPlayerId, secondPlayerId){
         this.id = id,
         this.firstPlayerId = firstPlayerId,
         this.secondPlayerId = secondPlayerId
     }
     
-
-    // this.createBattle = function(){
-
-    // };
+    var battleId = 1;
+    
     // images.map(x => x.id)
     this.getNextRound = function(players){
-        // TODO : return les battles de la ronde
         var battles = [];
         if (players.length == 1)
         {
             return { isFinished : true, winnerId : players[0], battles:battles};
         }
-
-        battles.push(new Battle(1, players[0], players[1]));
+        else if (players.length == 2)
+        {
+            battles.push(new Battle(battleId, players[0], players[1]));
+            battleId ++;
+        }
+        else
+        {
+            var totalPlayers = 0;
+            if (players % 2){
+                totalPlayers = players.length / 2;
+            }
+            else{
+                totalPlayers = (players.length - 1) / 2;
+            }
+            for(var index = 0; index < totalPlayers; index++)
+            {
+                battles.push(new Battle(battleId, players[index], players[index + totalPlayers]));
+                battleId ++;
+            }            
+        }
 
         return {
             isFinished:false,             
--- test/tournament.js
+++ test/tournament.js
@@ -14,7 +14,7 @@ describe('battle', () => {
         describe('Les participants sont divisés en deux sous groupes. Un sous groupe S1 composé de participants 1 à n/2, et un sous groupe s2 composé de participants (n/2)+1 à n. Le premier participant de S1 affronte le premier de S2, le second de S1 affronte le second de S2, etc.', () => {
           it('Pour 1 seul participant, il est déclaré vainqueur.', () => {
             var players = ['firstPlayerId'];  
-            var tournament = new Tournament();
+            var tournament = new Tournament(players);
             var nextRound = tournament.getNextRound(players);
             expect(nextRound.isFinished).to.be.true;
             expect(nextRound.battles.length).to.be.equal(0);
@@ -22,27 +22,58 @@ describe('battle', () => {
           });
           it('Pour 2 participants, les deux saffrontent.', () => {
             var players = ['firstPlayerId', 'secondPlayerId'];
-            var tournament = new Tournament();
+            var tournament = new Tournament(players);
             var nextRound = tournament.getNextRound(players);
             expect(nextRound.isFinished).to.be.false;
             expect(nextRound.battles.length).to.be.equal(1);
             expect(nextRound.battles[0].winnerId).to.be.undefined;
             expect(nextRound.battles[0].firstPlayerId).to.be.eql('firstPlayerId');
             expect(nextRound.battles[0].secondPlayerId).to.be.eql('secondPlayerId');
+            expect(nextRound.battles[0].id).to.be.equal(1);
           });
           it('Pour 3 participants, les deux premiers saffrontent, le dernier naffronte personne, mais il gagne 1 point.', () => {
             var players = ['firstPlayerId', 'secondPlayerId', 'thirdPlayerId'];
-            var tournament = new Tournament();
+            var tournament = new Tournament(players);
             var nextRound = tournament.getNextRound(players);
             expect(nextRound.isFinished).to.be.false;
             expect(nextRound.battles.length).to.be.equal(1);
             expect(nextRound.battles[0].winnerId).to.be.undefined;
             expect(nextRound.battles[0].firstPlayerId).to.be.eql('firstPlayerId');
             expect(nextRound.battles[0].secondPlayerId).to.be.eql('secondPlayerId');
+            expect(nextRound.battles[0].id).to.be.equal(1);
+            // third possède 1 point supplémentaire
+          });
+          it('Pour 11 participants, les dix premiers saffrontent, le dernier naffronte personne, mais il gagne 1 point.', () => {
+            var players = ['firstPlayerId', 'secondPlayerId', 'thirdPlayerId','fourthPlayerId', 'fifthPlayerId', 
+            'sixthPlayerId', 'seventhPlayerId', 'heighthPlayerId', 'ninthPlayerId','tenthPlayerId', 'eleventhPlayerId'];
+            var tournament = new Tournament(players);
+            var nextRound = tournament.getNextRound(players);
+            expect(nextRound.isFinished).to.be.false;
+            expect(nextRound.battles.length).to.be.equal(5);
+            expect(nextRound.battles[0].winnerId).to.be.undefined;
+            expect(nextRound.battles[0].firstPlayerId).to.be.eql('firstPlayerId');
+            expect(nextRound.battles[0].secondPlayerId).to.be.eql('sixthPlayerId');
+            expect(nextRound.battles[0].id).to.be.equal(1);
+            expect(nextRound.battles[1].firstPlayerId).to.be.eql('secondPlayerId');
+            expect(nextRound.battles[1].secondPlayerId).to.be.eql('seventhPlayerId');
+            expect(nextRound.battles[1].id).to.be.equal(2);
+            expect(nextRound.battles[2].firstPlayerId).to.be.eql('thirdPlayerId');
+            expect(nextRound.battles[2].secondPlayerId).to.be.eql('heighthPlayerId');
+            expect(nextRound.battles[2].id).to.be.equal(3);
+            expect(nextRound.battles[3].firstPlayerId).to.be.eql('fourthPlayerId');
+            expect(nextRound.battles[3].secondPlayerId).to.be.eql('ninthPlayerId');
+            expect(nextRound.battles[3].id).to.be.equal(4);
+            expect(nextRound.battles[4].firstPlayerId).to.be.eql('fifthPlayerId');
+            expect(nextRound.battles[4].secondPlayerId).to.be.eql('tenthPlayerId');
+            expect(nextRound.battles[4].id).to.be.equal(5);
+            // eleventh possède 1 point supplémentaire
           });
 
         });        
         it('En cas de nombre impair de participants le dernier est exempté et marque un point', () => {
+
+        });
+        it('Les identifiants des battles sont uniques.', () =>{
 
         });
         
